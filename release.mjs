e3c89b8761960eaa20c1d8a364a342dd70c94bb7
Tito Bouzout <tito.bouzout@gmail.com>
2025-05-28 16:59:20 -0300

    add release script

---
+++ release.mjs
@@ -0,0 +1,21 @@
+import { execSync as $ } from 'child_process'
+
+// bump version number
+$('npm version minor --git-tag-version false')
+
+// read version number
+import('./package.json', {
+	with: { type: 'json' },
+}).then(json => {
+	const version = json.default.version
+
+	// git add, commit with version number
+	$('git add --all')
+	$('git commit -m "v' + version + '"')
+	$('git tag "v' + version + '"')
+
+	// git push / npm publish
+	$('git push --all')
+	$('git push origin --tags')
+	$('npm publish')
+})
